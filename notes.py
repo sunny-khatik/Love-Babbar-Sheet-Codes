680d4e4c197293cc835fb9093de33b465db282e3
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-11 00:50:27 +0530

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,14 @@
+del : del is used to delete a reference to an object. Any variable or list value can be deleted using del.
+
+a = [1, 2, 3]
+  
+# printing list before deleting any value
+print ("The list before deleting any value")
+print (a)
+  
+# using del to delete 2nd element of list
+del a[1]
+  
+# printing list after deleting 2nd element
+print ("The list after deleting 2nd element")
+print (a)
