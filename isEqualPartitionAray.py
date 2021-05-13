56d9c3f2902886e24c6a8cd4982b4dab604f97b6
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-13 13:28:42 +0530

    Update isEqualPartitionAray.py

--- isEqualPartitionAray.py
+++ isEqualPartitionAray.py
@@ -8,19 +8,19 @@ class Solution:
         else:
             return self.isSubset(arr,len(arr),sum1//2)
     #using iterative solution     
-    def isSubsetIterative(self, arr,n,s):
+    def isSubset(self,arr,n, s):
         t = [[False for i in range(s+1)] for j in range(n+1)]
         for i in range(n+1):
-            for j in range(s+1):
-                if j == 0:
-                    t[i][0] = True
-                elif arr[i-1] <= j:
-                    t[i][j] = t[i-1][j-arr[i-1]] or t[i-1][j]
+            t[i][0]=True
+        for i in range(1,n+1):
+            for j in range(1,s+1):
+                if arr[i-1] <= j:
+                    t[i][j]= t[i-1][j-arr[i-1]] or t[i-1][j]
                 elif arr[i-1] > j:
                     t[i][j]=t[i-1][j]
-        if t[n][s] == 1:
+        if t[n][s]:
             return 1
-        else: 
+        else:
             return 0
     #using recursive approch
     def isSubset(self,arr,n, s):
