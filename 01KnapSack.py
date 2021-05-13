0907b5d744f407a226826a080f047d4d0398c945
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-14 00:41:44 +0530

    intialized matrix with -1 in iterative code
    intialized knapsack with -1 firstly in iterative approch

--- 01KnapSack.py
+++ 01KnapSack.py
@@ -1,15 +1,13 @@
 # itertive function
-class Solution:
-    
-    #Function to return max value that can be put in knapsack of capacity W.
-    def knapSack(self,c, wt, val, n):
-        t = [[0 for i in range(c+1)] for j in range(n+1)]
-        # print(t)
+def knapSack(self,c, wt, val, n):
+        t = [[-1 for i in range(c+1)] for j in range(n+1)]
         for i in range(n+1):
             for j in range(c+1):
                 if i == 0 or j == 0:
-                    t[i][j]=0
-                elif(wt[i-1] <= j):
+                    t[i][j] = 0
+        for i in range(1,n+1):
+            for j in range(1,c+1):
+                if(wt[i-1] <= j):
                     t[i][j]=max(val[i-1]+t[i-1][j-wt[i-1]] , t[i-1][j])
                 elif wt[i-1] > j:
                     t[i][j]=t[i-1][j]
