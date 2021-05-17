9e6b58673a8daeafe7e006fa2c6e7ce893977940
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-18 00:49:29 +0530

    Create eggdrop_problem.py

---
+++ eggdrop_problem.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+import sys
+def ikm():
+        return -1
+d = defaultdict(ikm)
+class Solution:
+    
+    #Function to find minimum number of attempts needed in 
+    #order to find the critical floor.
+    def eggDrop(self,n, k):
+        # code here
+        if n == 1:
+            return k
+        if k == 1 or k == 0:
+            return k
+        if d[str(n)+" "+str(k)] != -1:
+            return d[str(n)+" "+str(k)]
+        d[str(n)+" "+str(k)]  = sys.maxsize
+        
+        for j in range(1,k+1):
+            if d[str((n-1))+" "+str((j-1))] != -1:
+                e1 =  d[str((n-1))+" "+str((j-1))]
+            else:
+                e1 = self.eggDrop(n-1,j-1)
+                
+            if d[str(n)+" "+str(k-j)] != -1:
+                e2 = d[str(n)+" "+str(k-j)]
+            else:
+                e2 = self.eggDrop(n, k-j)
+            
+            tmp = 1 + max(e1 , e2) 
+            d[str(n)+" "+str(k)]  = min(d[str(n)+" "+str(k)]  , tmp)
+        return d[str(n)+" "+str(k)]  
