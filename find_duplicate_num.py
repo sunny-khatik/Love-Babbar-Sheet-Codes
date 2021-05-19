7299b4d52a14ace056447e3810964bc9ab432c6f
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-19 14:28:58 +0530

    Update find_duplicate_num.py

--- find_duplicate_num.py
+++ find_duplicate_num.py
@@ -14,3 +14,13 @@ class Solution:
             fast = nums[fast]
             slow = nums[slow]
         return slow
+
+    
+    #using array manipulation
+    class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if nums[abs(nums[i])] > 0:
+                nums[abs(nums[i])] = -nums[abs(nums[i])]
+            else:
+                return abs(nums[i])
