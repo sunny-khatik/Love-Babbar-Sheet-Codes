e8466a20de195329a8a1ae4263e5900373ff3471
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-20 15:40:41 +0530

    Update longest_substring_unique_char.py

--- longest_substring_unique_char.py
+++ longest_substring_unique_char.py
@@ -1,17 +1,22 @@
+# https://www.interviewbit.com/problems/longest-substring-without-repeat/
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
+# https://practice.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1#
 # https://youtu.be/qtVh-XEpsJo (see this for solution last approch)
 from collections import defaultdict 
-def assign():
-    return -1
-    
-d = defaultdict(assign)
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        left,right,ans,n = 0,0,0,len(s)
+    # @param A : string
+    # @return an integer
+    def assign(self):
+        return -1
+    def lengthOfLongestSubstring(self, A):
+        left,right,ans,n = 0,0,0,len(A)
+        d = defaultdict(self.assign)
         while right < n:
-            if d[s[right]] != -1:
-                left = max(left , d[s[right]]+1)
-            d[s[right]] = right
+            if d[A[right]] != -1:
+                left = max(left , d[A[right]]+1)
+            d[A[right]] = right
             ans = max(ans , right-left+1)
             right+=1
         return ans
+
+
