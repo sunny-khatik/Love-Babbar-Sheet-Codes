9d28561c6b7c3e28154b6e0686a7b9bcead32006
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-23 20:02:48 +0530

    Create longest_consecutive_subsequence.py

---
+++ longest_consecutive_subsequence.py
@@ -0,0 +1,28 @@
+# https://practice.geeksforgeeks.org/problems/longest-consecutive-subsequence2449/1#
+# see dhruv goyel solution
+class Solution:
+    
+    # arr[] : the input array
+    # N : size of the array arr[]
+    
+    #Function to return length of longest subsequence of consecutive integers.
+    def findLongestConseqSubseq(self,arr, N):
+        s = set()
+        c = 1
+        maxi = 1
+        for i in arr:
+            s.add(i)
+        for i in arr:
+            c=0
+            if i-1 not in s:
+                
+                tmp = i
+                # print(tmp)
+                while True:
+                    if tmp not in s:
+                        break
+                    tmp+=1
+                    c+=1
+                maxi = max(maxi , c)
+            
+        return maxi             
