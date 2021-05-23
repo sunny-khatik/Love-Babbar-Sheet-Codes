5b20f7289aada45ab34e8371c2537e5b24751dfb
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-23 13:46:43 +0530

    Create rearrange_positive_negative_InOrder_Inplace.py

---
+++ rearrange_positive_negative_InOrder_Inplace.py
@@ -0,0 +1,23 @@
+# see dhruv goyels's rearrange the array in alternate positive negative number in order in costant space
+l = [-5, -2, 5, 2, 4, 7, 1, 8, 0, -8 ]
+
+def rotateRight(l, start, stop):
+    start_ele = l[stop]
+    for i in range(stop, start, -1):
+        l[i]=l[i-1]
+    l[start]=start_ele
+n = len(l)
+wrongI = -1
+for i in range(n):
+    if wrongI != -1:
+        if (l[wrongI] >= 0 and l[i] < 0)  or (l[wrongI] < 0 and l[i]>= 0):
+            rotateRight(l,wrongI,i)
+            if i - wrongI >= 2:
+                wrongI+=2
+            else:
+                wrongI=-1
+    else:
+        if (l[i] < 0 and i%2 == 1 ) or( l[i]>= 0 and i%2 == 0):
+            wrongI = i
+            
+print(l)
