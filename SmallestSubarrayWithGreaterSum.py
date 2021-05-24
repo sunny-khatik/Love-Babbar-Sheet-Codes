29db2667a7b13c66e962f0a0d467f028b3339b72
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-24 16:06:06 +0530

    Create SmallestSubarrayWithGreaterSum.py

---
+++ SmallestSubarrayWithGreaterSum.py
@@ -0,0 +1,17 @@
+# https://practice.geeksforgeeks.org/problems/smallest-subarray-with-sum-greater-than-x5651/1#
+# see Dhruv Goyel's video for this.
+
+class Solution:
+    def sb(self, a, n, x):
+        ans=n+1
+        l ,r= 0,0
+        sum=0
+        while r < n:
+            while x >= sum and r < n:
+                sum+=a[r]
+                r+=1
+            while sum > x and l < n:
+                ans = min(ans , r-l)
+                sum-=a[l]
+                l+=1
+        return ans
