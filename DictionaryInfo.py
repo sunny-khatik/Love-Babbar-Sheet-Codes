7b75c149000b1cff16068e3e3931ec3e3e27909f
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-27 23:15:09 +0530

    Update and rename DictionarySort.py to DictionaryInfo.py

--- DictionaryInfo.py
+++ DictionaryInfo.py
@@ -4,3 +4,16 @@ d=sorted(d.items(),key=lambda x:x[0] , reverse=True) #sort the key of dictionary
 # put x[1] it will sort the dictionary using alue accordingly.
 d=sorted(d.items(),key=lambda x:x[1] , reverse=True) #sort dict by value in descending order
 d=sorted(d.items(),key=lambda x:x[1]) #sort dict by value in ascending order.
+
+
+#heap value dictionary
+#this counter return the occurance of chrachter return this dictionary.
+import collections
+s = "abcdefghi"
+cntr = collections.Counter(s)
+for i in cntr:
+    print(i, cntr[i])
+    
+    
+    
+
