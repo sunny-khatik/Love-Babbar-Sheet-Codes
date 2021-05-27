911d03e23bacab226a86dc59a2bbef8832eebe4c
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-27 19:22:12 +0530

    Create DictionarySort.py

---
+++ DictionarySort.py
@@ -0,0 +1,6 @@
+d = { "a" : 3 , "c" : 1, "b":0 , "g" : -4}
+d=sorted(d.items(),key=lambda x:x[0]) #sort the key of dictionary in ascending order
+d=sorted(d.items(),key=lambda x:x[0] , reverse=True) #sort the key of dictionary in descending orde
+# put x[1] it will sort the dictionary using alue accordingly.
+d=sorted(d.items(),key=lambda x:x[1] , reverse=True) #sort dict by value in descending order
+d=sorted(d.items(),key=lambda x:x[1]) #sort dict by value in ascending order.
