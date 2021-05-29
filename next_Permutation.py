99be5e6e5bb9c0b127d8a9d845552e7c32d39994
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-29 20:29:57 +0530

    Update next_Permutation.py

--- next_Permutation.py
+++ next_Permutation.py
@@ -1,27 +1,22 @@
 # https://leetcode.com/problems/next-permutation/
-
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        i= len(nums)-2
+    def nextPermutation(self, n, nums):
+        # code here
+        i= n-2
         while i > 0 and nums[i] >= nums[i+1]:
-            print(nums[i] , nums[i+1])
             i-=1
         swapVar=0
+        # print(i)
         for j in range(len(nums)-1 , i , -1):
-            # print(nums[i] , nums[j])
             if nums[i] < nums[j]:
                 swapVar = j
                 break
         if i == 0 and swapVar == 0:
-            return nums.reverse()
-        print(i , swapVar)
+            return nums[::-1]
         nums[i] , nums[swapVar] = nums[swapVar] , nums[i]
         r = (len(nums) - (i+1))//2
         for k in range(r):
             nums[k+i+1] , nums[len(nums)-1-k] = nums[len(nums)-1-k],nums[k+i+1]
-        return nums
+        return (nums)
                 
             
