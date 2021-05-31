e4a7b2ae8c91ab1bea2ff6cfdef6cea737b1d413
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-05-31 15:59:17 +0530

    Update nQueen.py

--- nQueen.py
+++ nQueen.py
@@ -1,59 +1,36 @@
 #best algo site for this'
 # http://techieme.in/solving-the-n-queen-problem/
-
-
-
-#  return Number of solution
+#print number of solution anarrengement also in 1D array
+l = list()
 class Solution:
-    def totalNQueens(self, n: int) -> int:
-        x = [0]*(n)
-        l = list()
-        self.nQueen(0,n,x,l)
-        return len(l)
-        
-    def nQueen(self,pos,n,x,l):
-        if pos == n:
-            l.append(x)
-            return
-        for i in range(0,n):
-            x[pos] = i+1
-            if self.place(x,pos):
-                self.nQueen(pos+1,n,x,l)
-            else:
-                x[pos]=-1
-                
-    def place(self,x ,pos):
-        for i in range(0,pos):
-            if x[i] == x[pos] or abs(x[i]-x[pos]) == abs(i-pos):
-                return False
-        return True
-      
- #print number of solution anarrengement also in 1D array
-l=list()
-class Solution:
+    
     def solveNQueens(self, n: int) -> List[List[str]]:
         x = [0]*(n)
-        self.nQueen(0,n,x)
-        print(len(l))
-        
-    def nQueen(self,pos,n,x):
+        ans=list()
+        self.nQueen(0,n,x,ans)
+        print("Number of Solution : ",len(l))
+        return ans
+    
+    def nQueen(self,pos,n,x,ans):
         if pos == n:
             print(x)
             l.append(x)
-            return
+            return 
         for i in range(0,n):
             x[pos] = i+1
-            if self.place(x,pos):
-                self.nQueen(pos+1,n,x)
-            else:
-                x[pos]=-1
+            if self.place(x,pos,ans):
+                self.nQueen(pos+1,n,x,ans)
                 
-    def place(self,x ,pos):
+    def place(self,x ,pos,ans):
         for i in range(0,pos):
             if x[i] == x[pos] or abs(x[i]-x[pos]) == abs(i-pos):
                 return False
-          
         return True
+
+
+            
+        
+        
       
 # https://leetcode.com/problems/n-queens/
 #leet code hard solution
