fd98293080502cfb35a8225af2d00433a41207fc
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-05 15:36:37 +0530

    Create LargestSubArrayWith0Sum.py

---
+++ LargestSubArrayWith0Sum.py
@@ -0,0 +1,16 @@
+#see striver solution
+from collections import defaultdict
+import sys
+def maxLen(n, arr):
+    d = defaultdict(lambda : -1)
+    prefsum=0
+    mx = 0
+    for i in range(n):
+        prefsum+=arr[i]
+        if prefsum == 0:
+            mx = max(mx , i+1)
+        elif d[prefsum] == -1:
+            d[prefsum] = i
+        else:
+            mx = max(mx , i-d[prefsum])
+    return mx
