836861f2320ef94342eb86c9a9f2eaf3b48f60ca
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-07 00:48:50 +0530

    Create FindPeakElement.py

---
+++ FindPeakElement.py
@@ -0,0 +1,14 @@
+#GFG Working Solution
+class Solution:   
+    def peakElement(self,arr, n):
+        # Code here
+        return self.peak(0, n-1, arr,n)
+    def peak(self,start , end, arr,n):
+        
+        mid = start +  (end-start)//2
+        if ((mid == 0 or arr[mid-1] <= arr[mid]) and (mid == n-1 or arr[mid+1] <= arr[mid])):
+            return mid
+        elif mid > 0 and arr[mid-1] > arr[mid]:
+            return self.peak(start , mid-1, arr,n)
+        else:
+            return self.peak(mid+1 , end, arr,n)
