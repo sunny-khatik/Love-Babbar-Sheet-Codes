ce982486a6418182885928caf7019364fac7b68a
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-08 14:13:46 +0530

    Update ArithmaticNumber.py

--- ArithmaticNumber.py
+++ ArithmaticNumber.py
@@ -1,6 +1,7 @@
 # https://practice.geeksforgeeks.org/problems/arithmetic-number2815/1#
 # code library 
 # Do checkout the solutions and article on it
+#number is there on series or not with given a and d.
 class Solution:
     def solve(self,mid,n):
         c , f=0,5
@@ -23,3 +24,21 @@ class Solution:
             else:
                 start=mid+1
         return ans
+    
+#find missing number in a arithmtic sequance.
+def findmissNum(start , end, arr,diff):
+    while start <= end:
+        mid = start + (end-start)//2
+        if mid+1 < n and arr[mid+1] != arr[mid]+diff:
+            return arr[mid]+diff
+        elif mid > 0 and arr[mid] != arr[mid-1]+diff:
+            return arr[mid-1]+diff
+        elif arr[mid] == arr[0]+mid*diff:
+            start = mid+1
+        else:
+            end=mid-1
+    return -1
+    
+arr = [2,4,8,10,12,14,16,18,20,22,24]
+n = len(arr)
+print(findmissNum(0,n-1,arr,(arr[n-1]-arr[0])//n))
