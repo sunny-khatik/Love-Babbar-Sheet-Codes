9c35a5b471b4202163e69fd3b3eee1447a727ca4
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-08 22:53:46 +0530

    Update merge_intervals.py

--- merge_intervals.py
+++ merge_intervals.py
@@ -15,6 +15,17 @@ class Solution:
                 tmp = i
         ans.append(tmp)
         return ans
-        
+ #O(1) space complexity
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        ind=0
+        for i in range(1,len(intervals)):
+            if intervals[ind][1] >= intervals[i][0]:
+                intervals[ind][1]=max(intervals[ind][1] ,intervals[i][1])
+            else:
+                ind+=1
+                intervals[ind]=intervals[i]
+        return intervals[:ind+1]
         
             
