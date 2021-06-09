259c5ba7c21fecb25e11bf8ec2d4aaf18ee51b07
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-09 22:48:02 +0530

    Update next_Permutation.py

--- next_Permutation.py
+++ next_Permutation.py
@@ -1,22 +1,24 @@
 # https://leetcode.com/problems/next-permutation/
+#TUF solution
 class Solution:
-    def nextPermutation(self, n, nums):
-        # code here
-        i= n-2
+    def nextPermutation(self, nums: List[int]) -> None:
+        i= len(nums)-2
         while i > 0 and nums[i] >= nums[i+1]:
             i-=1
         swapVar=0
-        # print(i)
         for j in range(len(nums)-1 , i , -1):
             if nums[i] < nums[j]:
                 swapVar = j
                 break
-        if i == 0 and swapVar == 0:
-            return nums[::-1]
+        if swapVar == 0:
+            return nums.reverse()
         nums[i] , nums[swapVar] = nums[swapVar] , nums[i]
-        r = (len(nums) - (i+1))//2
-        for k in range(r):
-            nums[k+i+1] , nums[len(nums)-1-k] = nums[len(nums)-1-k],nums[k+i+1]
-        return (nums)
+        start = i+1
+        end = len(nums)-1
+        while start < end:
+            nums[start], nums[end]=nums[end],nums[start]
+            start+=1
+            end-=1
+        return nums
                 
             
