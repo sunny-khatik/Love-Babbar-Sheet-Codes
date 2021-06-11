89abb1e4c00f73def464db736089b71f6bcf8424
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-11 19:09:30 +0530

    Create IntersectionTwoList.py

---
+++ IntersectionTwoList.py
@@ -0,0 +1,18 @@
+def findIntersection(a , b):
+    res = None
+    prev = None
+    while a and b :
+        if a.data == b.data:
+            new = Node(a.data)
+            if res == None:
+                res=new
+            else:
+                prev.next=new
+            prev=new
+            a=a.next
+            b=b.next
+        elif a.data > b.data:
+            b=b.next
+        else:
+            a=a.next
+    return res
