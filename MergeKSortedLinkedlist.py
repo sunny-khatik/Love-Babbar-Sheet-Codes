de167fedf787862ce09896cc5e833855726d5e6a
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-14 11:48:26 +0530

    Create MergeKSortedLinkedlist.py

---
+++ MergeKSortedLinkedlist.py
@@ -0,0 +1,28 @@
+# https://practice.geeksforgeeks.org/problems/merge-k-sorted-linked-lists/1
+def merge(self, a,b):
+        if a == None:
+            return b
+        if b == None:
+            return a
+        res = None
+        if a.data <= b.data:
+            res = a
+            res.next = self.merge(a.next , b)
+        else:
+            res = b
+            res.next = self.merge(a, b.next)
+        return res
+    #Function to merge K sorted linked list.
+    def mergeKLists(self,arr,k):
+        last = k-1
+        i = 0
+        while last != 0:
+            i = 0
+            j = last
+            while i < j:
+                arr[i]=self.merge(arr[i] , arr[j])
+                i+=1
+                j-=1
+                if i>= j:
+                    last=j
+        return arr[0]
