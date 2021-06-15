867d223b9e1f48ca3c2e9045b291a6a95dd6ec3b
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-15 22:55:11 +0530

    Create LevelOrderTraversal.py

---
+++ LevelOrderTraversal.py
@@ -0,0 +1,22 @@
+# https://practice.geeksforgeeks.org/problems/level-order-traversal/1#
+class Solution:
+    #Function to return the level order traversal of a tree.
+    def solve(self,root,l):
+        if root == None:
+            l.append("N")
+            return 
+        queue = list()
+        queue.append(root)
+        while len(queue) > 0:
+            l.append(queue[0].data)
+            node = queue.pop(0)
+            
+            if node.right != None:
+                queue.append(node.right)
+            if node.left != None:
+                queue.append(node.left)
+                
+    def levelOrder(self,root ):
+        l= list()
+        self.solve(root,l)
+        return l
