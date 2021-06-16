ec78e9213ec99844e82402720a86036401fcbce8
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-16 14:04:38 +0530

    Update TreeTraversal.py

--- TreeTraversal.py
+++ TreeTraversal.py
@@ -4,6 +4,46 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
+# def printPostorder(root):
+#     if root == None:
+#         return
+#     ans = list()
+#     stack = list()
+#     stack.append(root)
+#     while len(stack) > 0:
+#         node = stack.pop()
+#         ans.append(node.data)
+#         if node.left:
+#             stack.append(node.left)
+#         if node.right:
+#             stack.append(node.right)
+#     while ans:
+#         print(ans.pop(),end=" ")
+# def printInorder(root):
+#     if root == None:
+#         return
+#     stack = list()
+#     while len(stack) > 0 or root != None:
+#         if root:
+#             stack.append(root)
+#             root=root.left
+#         else:
+#             root = stack.pop(-1)
+#             print(root.data,end=" ")
+#             root = root.right
+    
+# def printPreorder(root):
+#     if root == None:
+#         return
+#     stack = list()
+#     stack.append(root)
+#     while len(stack) > 0:
+#         node = stack.pop()
+#         print(node.data , end=" ")
+#         if node.right != None:
+#             stack.append(node.right)
+#         if node.left != None:
+#             stack.append(node.left)     
 def printPostorder(root):
     if root != None:
         printPostorder(root.left)
@@ -18,7 +58,7 @@ def printInorder(root):
         printInorder(root.right)
     else:
         return
-    
+
 def printPreorder(root):
     if root != None:
         print(root.data, end=" ")
@@ -27,7 +67,6 @@ def printPreorder(root):
         printPreorder(root.right)
     else:
         return
-    
 
 root = Node(1)
 root.left = Node(2)
