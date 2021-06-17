ebd005b23fc0f301945891eaf065101fe4f76618
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-17 18:21:41 +0530

    Update QuickSortOpt.py

--- QuickSortOpt.py
+++ QuickSortOpt.py
@@ -26,6 +26,7 @@ print(l)
 
 #just go for random pivot it is optimized then
 import random
+import copy
 def Quicksort(l, lb , up):
     if lb < up:
         pivot = findPiv(l, lb,up)
@@ -33,7 +34,9 @@ def Quicksort(l, lb , up):
         Quicksort(l, pivot+1,up)
         
 def findPiv(l,start,end):
-    pivot = l[random.randint(start,end)]
+    randomNum = random.randint(start,end)
+    l[start],l[randomNum]=l[randomNum],l[start]
+    pivot = l[start]
     tmp = start
     while start < end:
         while start < len(l) and l[start] <= pivot:
@@ -44,7 +47,6 @@ def findPiv(l,start,end):
             l[start],l[end]=l[end],l[start]
     l[end] , l[tmp]=l[tmp],l[end]
     return end
-
-l = [7,10,2,1,9,10,15,12]
-Quicksort(l, 0,len(l)-1)
-print(l)
+l = [-1,-10,5,3,6,23,4,6,78,7,-1,-15]
+Quicksort(l,0,len(l)-1)
+print(*l)
