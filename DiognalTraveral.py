581f1b997bc601825ad156ef9c1ef9f5331947fc
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-19 18:30:54 +0530

    Create DiognalTraveral.py

---
+++ DiognalTraveral.py
@@ -0,0 +1,13 @@
+# https://practice.geeksforgeeks.org/problems/diagonal-traversal-of-binary-tree/1
+def diagonal(root):
+    ans = list()
+    q = list()
+    q.append(root)
+    while len(q) > 0:
+        root = q.pop(0)
+        while root != None:
+            if root.left != None:
+                q.append(root.left)
+            ans.append(root.data)
+            root=root.right
+    return ans
