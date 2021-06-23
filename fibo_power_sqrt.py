552a14a572bcbf05405f6f5182fef257758bf102
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2021-06-23 15:27:46 +0530

    Update fibo_power_sqrt.py

--- fibo_power_sqrt.py
+++ fibo_power_sqrt.py
@@ -16,8 +16,61 @@ def solve(a,b):
 
 x, y = 8, -1  #works for both the cases.
 print((solve(x, y)))
-
-
+#another power version with mod recursive
+class Solution:
+    # (x^n)%d
+    def pow(self, x, n, d):
+        x = x%d
+        if x == 0:
+            return 0
+        return self.solve(x,n,d)
+    
+    def solve(self, a,b,MOD):
+        if b == 0:
+            return 1
+        tmp = self.solve(a,b//2,MOD)
+        tmp=  tmp%MOD
+        if b&1:
+            return (tmp*tmp*a)%MOD
+        else:
+            return (tmp*tmp)%MOD
+        
+#power with mod iterative
+class Solution:
+    # (x^n)%d
+    def pow(self, x,y,p):
+        x = x%p
+        if x == 0:
+            return 0
+        res = 1
+        while y >0:
+            if y&1 == 1:
+                res = (res*x)%p
+            y = y >> 1
+            x = (x*x)%p
+        return (res)
+        
+        
+#sqrt of int return floor value
+import math
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def sqrt(self, A):
+        start = 0
+        end = A
+        ans= 0
+        for i in range(100):
+            mid = (start)+ ((end-start)/2)
+            if mid*mid == A:
+                ans=mid
+                break
+            elif mid*mid > A:
+                end=mid-1
+            else:
+                ans = mid
+                start = mid+1
+        return math.floor(ans)
 
 
 # nth fibonachi number using O(logn) approch
